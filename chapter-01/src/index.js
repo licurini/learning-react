d492d7f816a0a9da36d6a1a0128b8a7bcaaf5eee
licurini <licurini@gmail.com>
2017-11-09 19:11:02 +0100

    Cars.js created

--- chapter-01/src/index.js
+++ chapter-01/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Cars from './Cars.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Cars />, document.getElementById('root'));
 registerServiceWorker();
