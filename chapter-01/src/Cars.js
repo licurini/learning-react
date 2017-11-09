83918c058fdf64532e70a0217c13902e8f5d8d5f
licurini <licurini@gmail.com>
2017-11-09 21:33:59 +0100

    Array passed to components initial state

--- chapter-01/src/Cars.js
+++ chapter-01/src/Cars.js
@@ -2,17 +2,64 @@ import React, { Component } from 'react';
 import './Cars.css';
 
 class Cars extends Component {
-  constructor (props) {
-	  super ();
+    constructor (props) {
+	    super ();
 	  
-  }
+	//pomijam getInitialState, bo przy takiej formie inicjalizacji komponentu podobno ta metoda nie jest zalecana.
+    
+		this.state = {
+		    cartypes: [{
+			    brand: "Honda",
+				type: "liftback"
+		    },
+			{
+				brand: "Audi",
+				type: "hatchback"
+			},
+			{
+				brand: "Mercedes",
+				type: "sedan"
+			},
+			{
+				brand: "Land Rover",
+				type: "4WD"
+			},
+			{
+				brand: "Alfa Romeo",
+				type: "alwaysbroken"
+			},
+			{
+			    brand: "Porshe",
+				type: "twoseater"
+			},
+			{
+				brand: "Ferrari",
+				type: "supersport"
+			},
+			{
+				brand: "Jeep",
+				type: "offroad"
+			},
+			{
+				brand: "Koenigsegg",
+				type: "premium"
+			},
+			{
+				brand: "Jaguar",
+				type: "limo"
+			},
+		    ]
+	    }
+		
+		console.log("Component initialized. Initial state is set.")
+    }
   
   
-  render() {
-    return (
-      <h1>{this.props.title}</h1>
-    );
-  }
+    render() {
+      return (
+        <h1>{this.props.title}</h1>
+      );
+    }
 }
 
 export default Cars;
