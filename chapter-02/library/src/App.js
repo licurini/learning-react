d339a5c571f6e44e7ba9ee30eae6b6e4cbd3b586
licurini <licurini@gmail.com>
2017-12-05 21:34:13 +0100

    Started routing

--- chapter-02/library/src/App.js
+++ chapter-02/library/src/App.js
@@ -6,10 +6,24 @@ import {
     Link
 } from 'react-router-dom';
 
+import Profile from './Profile.js';
+import Books from './Books.js';
+
 class App extends Component {
   render() {
     return (
-        null
+        <Router>
+            <div className="container">
+                <div>
+                    <h1>Nawigacja</h1>
+                    <Link to="/profile">Profil</Link> <br />
+                    <Link to="/books">Książki</Link>
+                </div>
+
+                <Route path="/profile" component={Profile} />
+                <Route path="/books" component={Books} />
+            </div>
+        </Router>
     );
   }
 }
--- chapter-02/library/src/index.js
+++ chapter-02/library/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Books from './Books.js';
+import App from './App.js';
 
-ReactDOM.render(<Books />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
