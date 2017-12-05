987a0721ddcb77236e918a843b95a08ef570bbb3
licurini <licurini@gmail.com>
2017-12-05 18:27:02 +0100

    updated profile

--- chapter-02/library/src/ProfileAvatar.js
+++ chapter-02/library/src/ProfileAvatar.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
-
+import './ProfileAvatar.css';
 
 class ProfileAvatar extends Component {
-  render() {
-    return (
-        <div>
-            <h1>Profile1</h1>
-            <img src={require('./img/avatar1.png')} />
-        </div>
-    );
-  }
-}
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            booksOnLoan: [
+                {
+                    id: "1",
+                    title: "Hobbit",
+                    author: "J. R. R. Tolkien"
+                },
+                {
+                    id: "2",
+                    title: "Learning Python",
+                    author: "Unknown"
+                },
+                {
+                    id: "3",
+                    title: "Kamasutra",
+                    author: "Unknown"
+                },
+                {
+                    id: "4",
+                    title: "Dzieci z Bullerbyn",
+                    author: "Astrid Lindgren"
+                },
+                {
+                    id: "5",
+                    title: "Kapitał"
+                    author: "Karol Marks"
+                },
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Profile1</h1>
+                <img src={require('./img/avatar1.png')} />
+            </div>
+        );
+      }
+    }
 
 export default ProfileAvatar;
